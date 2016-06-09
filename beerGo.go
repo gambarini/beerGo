68c679e920432f009ae6455dcd7c708096790d93
Gustavo Gambarini <gambarini@gmail.com>
2016-06-10 01:02:47 +1000

    initial commit

---
+++ beerGo.go
@@ -0,0 +1,115 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"strconv"
+
+	"github.com/julienschmidt/httprouter"
+	"gopkg.in/couchbase/gocb.v1"
+)
+
+//GetBeer ok
+func GetBeer(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+
+	limit, offset := GetLimitAndOffset(r.URL.Query().Get("limit"), r.URL.Query().Get("offset"))
+
+	json, err := QueryBeerSample("beer", limit, offset)
+
+	HandleResponse(json, err, w)
+
+}
+
+//GetBrewery ok
+func GetBrewery(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+
+	limit, offset := GetLimitAndOffset(r.URL.Query().Get("limit"), r.URL.Query().Get("offset"))
+
+	json, err := QueryBeerSample("brewery", limit, offset)
+
+	HandleResponse(json, err, w)
+
+}
+
+//Test ok
+func Test(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	fmt.Fprint(w, "OK!")
+}
+
+// HandleResponse ok
+func HandleResponse(json []byte, err error, w http.ResponseWriter) {
+	if err != nil {
+		w.WriteHeader(500)
+		fmt.Fprintf(w, "%s", err)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(200)
+
+	fmt.Fprintf(w, "%s", json)
+}
+
+// GetLimitAndOffset ok
+func GetLimitAndOffset(strLimit, strOffset string) (int, int) {
+	var limit, offset int
+	var errLimit, errOffset error
+
+	if limit, errLimit = strconv.Atoi(strLimit); errLimit != nil {
+		limit = 10
+	}
+
+	if offset, errOffset = strconv.Atoi(strOffset); errOffset != nil {
+		offset = 0
+	}
+
+	return limit, offset
+}
+
+// QueryBeerSample ok
+func QueryBeerSample(sampleType string, limit, offset int) ([]byte, error) {
+
+	sQuery := fmt.Sprintf("SELECT b.*, meta(b).id as id FROM `beer-sample` b where b.type='%s' order by b.name limit %d offset %d", sampleType, limit, offset)
+	myQuery := gocb.NewN1qlQuery(sQuery)
+	rows, err := bucket.ExecuteN1qlQuery(myQuery, nil)
+
+	if err != nil {
+		return nil, err
+	}
+
+	var retValues []interface{}
+	var row interface{}
+
+	for rows.Next(&row) {
+		retValues = append(retValues, row)
+	}
+
+	json, err := json.Marshal(retValues)
+
+	return json, err
+}
+
+var bucket *gocb.Bucket
+
+func main() {
+	// Connect to Cluster
+	cluster, err := gocb.Connect("couchbase://127.0.0.1")
+	if err != nil {
+		fmt.Println("ERRROR CONNECTING TO CLUSTER:", err)
+	}
+	// Open Bucket
+	bucket, err = cluster.OpenBucket("beer-sample", "")
+	if err != nil {
+		fmt.Println("ERRROR OPENING BUCKET:", err)
+	}
+
+	r := httprouter.New()
+
+	r.GET("/api/beer", GetBeer)
+	r.GET("/api/brewery", GetBrewery)
+
+	r.ServeFiles("/web/*filepath", http.Dir("web"))
+
+	http.ListenAndServe("localhost:9000", r)
+}
---
+++ web/app.jsx
@@ -0,0 +1,73 @@
+"use strict";
+
+var BeerBox = React.createClass({
+  getInitialState: function() {
+    return {data: []};
+  },
+  componentDidMount: function() {
+    $.ajax({
+      url: this.props.url,
+      dataType: 'json',
+      cache: false,
+      success: function(data) {
+        this.setState({data: data});
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
+  },
+  render: function() {
+    return (
+      <div className="beerBox">
+        <h1>Beer</h1>
+        <BeerList data={this.state.data}/>
+      </div>
+    );
+  }
+});
+
+var BeerList = React.createClass({
+  render: function() {
+    var beersInfo = this.props.data.map(function(beer) {
+      return (
+        <BeerInfo name={beer.name} style={beer.style} key={beer.id}>
+          {beer.description}
+        </BeerInfo>
+      );
+    });
+    return (
+      <div className="beerList">
+        {beersInfo}
+      </div>
+    );
+  }
+});
+
+var BeerInfo = React.createClass({
+  rawMarkup: function() {
+      var md = new Remarkable();
+      var rawMarkup = md.render(this.props.children.toString());
+      return { __html: rawMarkup };
+    },
+
+  render: function() {
+    var md = new Remarkable();
+    return (
+      <div className="beerInfo">
+        <h2 className="BeerInfoHeader">
+          {this.props.name}
+        </h2>
+        <h4>
+          {this.props.style}
+        </h4>
+        <span dangerouslySetInnerHTML={this.rawMarkup()} />
+      </div>
+    );
+  }
+});
+
+ReactDOM.render(
+  <BeerBox url="/api/beer" />,
+  document.getElementById('content')
+);
